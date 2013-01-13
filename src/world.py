1fd5b76f12bfc978b3601bf3aaf29a2e2c132528
Colton Myers <colton.myers@gmail.com>
2013-01-12 21:12:29 -0700

    Add world.py

---
+++ src/world.py
@@ -0,0 +1,31 @@
+'''
+World generation tools and representation
+'''
+
+
+class World(object):
+    '''
+    Represents the world
+
+    Member Variables
+        seed: the random seed for this world
+    '''
+
+    def __init__(self, seed):
+        self.seed = seed
+
+    def generate_tile(self, x, y):
+        pass
+
+
+class Pane(object):
+    '''
+    Represents a single screen of the world
+
+    Member Variables
+        tiles: Dictionary of coordinate tuples (e.g. (0,1)) to tile objects
+    '''
+
+    def __init__(self):
+        self.tiles = dict()
+
