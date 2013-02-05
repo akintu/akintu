e11eca3d8650955c763633fc83da8bbcc072bb2b
BlackVegetable <devinj.ekins@gmail.com>
2013-02-05 14:40:30 -0700

    Added most of on hit effects.
    On hit effects will need to be registered when they are equipped (or
    granted via a buff) and unregistered afterward.
    
    Most of this class is complete, but I need seven more effects.  It will
    need to be integrated with the magic properties as well.

---
+++ src/onhiteffect.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+import sys
+import dice
+
+class OnHitEffect(object):
+    def __init__(self):
+	    pass
+		
+    def applyAcidic(self, magnitude, source, target):
+        # Magnitude determines chance.
+        chance = magnitude
+        if Dice.rollBeneath(chance):
+            duration = 4
+            Combat.addStatus(target, "Acidic Magic Weapon", duration)
+        return None
+        
+    def applyEvil(self, magnitude, source, target):        
+        # Magnitude determines damage.
+        HPLoss = round(source.totalHP * 0.08)
+        if HPLoss >= source.HP:
+            HPLoss = source.HP - 1
+        Combat.modifyResource(source, "HP", -HPLoss)
+        damage = magnitude * 12
+        return ["Shadow", damage]
+        
+    def applyHoly(self, magnitude, source, target):
+        # Magnitude determines chance.
+        chance = magnitude
+        duration = -1 # Does not end.
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(target, "Holy Magic Weapon", duration)
+            if Dice.rollBeneath(50):
+                Combat.addStatus(target, "Stun", 1)
+            return ["Divine", 20]
+        else:
+            return None
+            
+    def applyIgnite(self, magnitude, source, target):
+        # magnitude determines damage.
+        duration = 3
+        damageMag = Dice.roll(1, 5) * magnitude
+        Combat.addStatus(target, "Ignite Magic Weapon", duration, damageMag)
+        return None
+        
+    def applySlowing(self, magnitude, source, target):
+        # Magnitude determines chance.
+        duration = 2
+        chance = magnitude
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(target, "Slowing Magic Weapon", duration)
+        return None
+        
+    def applySpellhunger(self, magnitude, source, target):
+        # Magnitude determines MP destroyed.
+        MPLoss = magnitude
+        target.MP -= MPLoss
+        return None
+        
+    def applyStunning(self, magnitude, source, target):
+        # Magnitude determines chance.
+        chance = max(20, magnitude)
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(target, "Stun", 1)
+        return None
+        
+    def applyMinorBleeding(self, magnitude, source, target):
+        # Magnitude increases chance.
+        chance = magnitude
+        duration = 3
+        magnitude = 5
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(target, "Bleeding", duration, magnitude, overwrite=False)
+        return None
+        
+    def applyModerateBleeding(self, magnitude, source, target):
+        # Magnitude increases chance.
+        chance = magnitude
+        duration = 3
+        magnitude = 10
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(target, "Bleeding", duration, magnitude, overwrite=False)
+        return None
+        
+    def applySeriousBleeding(self, magnitude, source, target):
+        # Magnitude increases chance.
+        chance = magnitude
+        duration = 3
+        magnitude = 15
+        if Dice.rollBeneath(chance):
+            Combat.addStatus(taget, "Bleeding", duration, magnitude, overwrite=False)
+        # TODO: Make different sources of bleeding stack.
+        return None
+        
+    def applyHealthSteal(self, magnitude, source, target):
+        # Magnitude increases amount.
+        amount = magnitude
+        Combat.healTarget(source, source, min(target.HP, amount))
+        return ["Divine", amount]
+        
+    def applyManaSteal(self, magnitude, source, target):
+        amount = magnitude
+        restoration = min(amount, target.MP)
+        Combat.modifyResource(target, "MP", -amount)
+        Combat.modifyResource(source, "MP", restoration)
+        return None
+        
+    def applyToxic(self, magnitude, source, target):
+        pRating = magnitude + 5
+        damage = Dice.roll(2,4)
+        duration = 5
+        hitType = Combat.calcHit(source, target, "Poison", rating=pRating)
+        if hitType != "Miss":
+            Combat.addStatus(target, "Toxic Magic Weapon", duration, damage)
+        return None
+    
+    
+    
