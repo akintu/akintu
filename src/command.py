9c96815f7bd3e8bd8691e5ac53d8c6b65f5ca150
Kyle Rich <yutakafrog@gmail.com>
2013-02-08 03:52:40 -0700

    Remove extraneous classes
    Also redo the Command class __repr__ method.  (Delete a single
    extraneous space character, lol)

--- src/command.py
+++ src/command.py
@@ -5,42 +5,33 @@ Class for communicating map sync actions
 def enum(**enums):
     return type('Enum', (), enums)
 
+PersonActions = enum(CREATE = 1, MOVE = 2, REMOVE = 3)
 InventoryActions = enum(PICKUP = 1, PUTDOWN = 2)
 
+
 class Command:
-    def __init__(self):#, hash, location, action):
+    def __init__(self):
         pass
-        
+
     def __repr__(self):
         result = "(" + self.__class__.__name__ + ": ["
-        for attr, val in self.__dict__.iteritems():
-            result += attr + ": " + str(val) + " "
+        #for attr, val in self.__dict__.iteritems():
+        #    result += attr + ": " + str(val) + " "
+        result += ", ".join([attr + ": " + str(val) for attr, val in self.__dict__.iteritems()])
         result += "])"
         return result
-        
-class CreatePerson(Command):
-    def __init__(self, index, location):
+
+class Person(Command):
+    def __init__(self, action, index, location):
+        self.action = action
         self.index = index
         self.location = location
-        
-class MovePerson(Command):
-    def __init__(self, index, dest):
-        self.index = index
-        self.dest = dest
-        
-class RemovePerson(Command):
-    def __init__(self, index):
-        self.index = index
-        
-class UpdateIndex(Command):
-    def __init__(self, index):
-        self.index = index
-    
+
 class InventoryAction(Command):
     def __init__(self, action, item_hash):
         self.action = action
         self.item_hash = item_hash
-        
+
 class AbilityAction(Command):
     def __init__(self, ability, targetLocation):
         self.ability = ability
