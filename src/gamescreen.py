cbf0213805378f36fe370c6fe26880d86033ff19
Colton Myers <colton.myers@gmail.com>
2013-02-23 16:13:31 -0700

    Add player unit frames (big commit)

--- src/gamescreen.py
+++ src/gamescreen.py
@@ -8,7 +8,7 @@ from pygame import Rect, Color
 
 import os
 import sys
-import collections
+from collections import OrderedDict
 from PIL import Image
 
 from const import *
@@ -24,16 +24,17 @@ class GameScreen(object):
         '''
         Initialize Akintu graphics engine with default settings
         '''
-        self.screen = pygame.display.set_mode(
-                (PANE_X * TILE_SIZE + 256,
-                max(PANE_Y * TILE_SIZE, 20 * TILE_SIZE)
-                ))
+        self.screen = pygame.display.set_mode((
+            PANE_X * TILE_SIZE + 256,
+            max(PANE_Y * TILE_SIZE, 20 * TILE_SIZE)))
         pygame.display.set_caption('Akintu r01')
         self.background = pygame.Surface((PANE_X * TILE_SIZE,
                                           PANE_Y * TILE_SIZE))
         self.images = dict()
         self.persons = OrderedDict()
         self.personsgroup = pygame.sprite.RenderUpdates()
+        self.playerframes = OrderedDict()
+        self.monsterframes = OrderedDict()
         pygame.display.flip()
 
     def set_pane(self, pane):
@@ -44,6 +45,8 @@ class GameScreen(object):
         for personid in list(self.persons):
             self.personsgroup.remove(self.persons[personid])
             self.persons.pop(personid)
+        self.playerframes = OrderedDict()
+        self.monsterframes = OrderedDict()
         for key, val in pane.images.iteritems():
             mode = val.mode
             size = val.size
@@ -62,6 +65,7 @@ class GameScreen(object):
         '''
         Draw the world (represented by a Pane)
         '''
+        self.screen.fill(Color('black'))
         for i, j in [(i, j) for i in range(PANE_X) for j in range(PANE_Y)]:
             tile = self.pane.tiles[(i, j)]
 
@@ -152,12 +156,23 @@ class GameScreen(object):
         self.persons[personid] = PersonSprite(statsdict)
         self.personsgroup.add(self.persons[personid])
 
+        if statsdict['team'] == "Players":
+            self.playerframes[personid] = \
+                self._generateplayerframe(personid)
+            self._drawplayerframes()
+        elif statsdict['team'] == "Monsters":
+            #self.monsterframes[personid] = \
+                #self._generatemonsterframe(personid)
+            #self._drawmonsterframes()
+            pass
+
     def remove_person(self, personid):
         '''
         Remove a Person object from the world
         '''
         self.personsgroup.remove(self.persons[personid])
         self.persons.pop(personid)
+        self.playerframes.pop(personid)
 
     def update_person(self, personid, statsdict):
         '''
@@ -169,6 +184,13 @@ class GameScreen(object):
         if 'location' in statsdict:
             self.persons[personid].current_coord = statsdict['location']
         self.persons[personid].update_dict(statsdict)
+
+        for key in statsdict:
+            if key in ['name', 'HP', 'MP', 'AP', 'buffedHP', 'totalHP',
+                       'totalMP', 'totalAP']:
+                self.playerframes[personid] = \
+                        _generateplayerframe(personid)
+                self._drawplayerframes()
 
     def update(self):
         '''
@@ -187,6 +209,113 @@ class GameScreen(object):
         '''
         if SHOW_FPS:
             pygame.display.set_caption("%s %d" % (CAPTION, fps))
+
+    def _generateplayerframe(self, personid):
+        frame = pygame.Surface((246, 70))
+        frame.fill(Color('gray'))
+        image = self.persons[personid].images[0][2]
+        statsdict = self.persons[personid].statsdict
+
+        if 'name' not in statsdict:
+            name = 'Mysterious Adventurer'
+        else:
+            name = statsdict['name']
+
+        # Retrieve stats (error-checking as we go)
+        if 'totalHP' not in statsdict or statsdict['totalHP'] <= 0:
+            totalHP = None
+        else:
+            totalHP = statsdict['totalHP']
+        if 'HP' not in statsdict or not totalHP:
+            HP = 0
+        else:
+            HP = statsdict['HP']
+        if 'buffedHP' not in statsdict or not totalHP:
+            buffedHP = 0
+        else:
+            buffedHP = statsdict['buffedHP']
+        if 'totalMP' not in statsdict or statsdict['totalMP'] <= 0:
+            totalMP = None
+        else:
+            totalMP = statsdict['totalMP']
+        if 'MP' not in statsdict or not totalMP:
+            MP = 0
+        else:
+            MP = statsdict['MP']
+        if 'totalAP' not in statsdict or statsdict['totalAP'] <= 0:
+            totalAP = None
+        else:
+            totalAP = statsdict['totalAP']
+        if 'AP' not in statsdict or not totalAP:
+            AP = 0
+        else:
+            AP = statsdict['AP']
+
+        hstring = str(HP) + ' / ' + (str(totalHP) if totalHP else '0')
+        mstring = str(MP) + ' / ' + (str(totalMP) if totalMP else '0')
+        astring = str(AP) + ' / ' + (str(totalAP) if totalAP else '0')
+
+        barx = 32 + 4 + 40
+        barlabelx = 32 + 4 + 4
+        barxsize = 166
+        barysize = 10
+        hbary = 16
+        if totalHP:
+            hbar1 = barxsize
+            hbar2 = int(HP / totalHP * barxsize)
+            hbar3 = int(buffedHP / totalHP * barxsize)
+        else:
+            hbar1 = barxsize
+            hbar2 = 0
+            hbar3 = 0
+        mbary = hbary + 12
+        if totalMP:
+            mbar1 = barxsize
+            mbar2 = int(MP / totalMP * barxsize)
+        else:
+            mbar1 = barxsize
+            mbar2 = 0
+        abary = mbary + 12
+        if totalAP:
+            abar1 = barxsize
+            abar2 = int(AP / totalAP * barxsize)
+        else:
+            abar1 = barxsize
+            abar2 = 0
+
+        frame.blit(image, (4, 4))
+        frame.fill(Color('black'), (barx, hbary, hbar1, barysize))
+        frame.fill(Color('red'), (barx, hbary, hbar2, barysize))
+        frame.fill(Color('darkred'), (barx, hbary, hbar3, barysize))
+        frame.fill(Color('black'), (barx, mbary, mbar1, barysize))
+        frame.fill(Color('blue'), (barx, mbary, mbar2, barysize))
+        frame.fill(Color('black'), (barx, abary, abar1, barysize))
+        frame.fill(Color('purple'), (barx, abary, abar2, barysize))
+
+        font = pygame.font.SysFont("Arial", 10)
+        hfont = font.render(hstring, True, Color('black'))
+        mfont = font.render(mstring, True, Color('black'))
+        afont = font.render(astring, True, Color('black'))
+        frame.blit(hfont, (barlabelx, hbary))
+        frame.blit(mfont, (barlabelx, mbary))
+        frame.blit(afont, (barlabelx, abary))
+
+        font = pygame.font.SysFont("Arial", 12, bold=True)
+        namefont = font.render(name, True, Color('black'))
+        frame.blit(namefont, (barx, 2))
+
+        return frame
+
+    def _drawplayerframes(self):
+        top = 4
+        left = PANE_X * TILE_SIZE + 5
+        rect = (left, top, 245, 296)
+        self.screen.fill(Color('black'), rect)
+
+        for frame in self.playerframes:
+            self.screen.blit(self.playerframes[frame], (left, top))
+            top += 74
+        pygame.display.update([rect])
 
 
 class PersonSprite(pygame.sprite.DirtySprite):
@@ -209,13 +338,13 @@ class PersonSprite(pygame.sprite.DirtySprite):
             endings = [(2, '_fr1.png'), (4, '_lf1.png'),
                        (6, '_rt1.png'), (8, '_bk1.png')]
             self.images.append(
-                    {key: pygame.image.load(imagepre+end).convert_alpha()
-                    for key, end in endings})
+                {key: pygame.image.load(imagepre+end).convert_alpha()
+                 for key, end in endings})
             endings = [(2, '_fr2.png'), (4, '_lf2.png'),
                        (6, '_rt2.png'), (8, '_bk2.png')]
             self.images.append(
-                    {key: pygame.image.load(imagepre+end).convert_alpha()
-                    for key, end in endings})
+                {key: pygame.image.load(imagepre+end).convert_alpha()
+                 for key, end in endings})
         # Assume imagepre is the actual image location, add facing overlays
         except pygame.error:
             self.images = facing_overlays(imagepre)
@@ -223,8 +352,8 @@ class PersonSprite(pygame.sprite.DirtySprite):
         # Location and rect info
         loc = self.statsdict['location']
         foot = self.statsdict['foot']
-        xoff= self.statsdict['xoffset']
-        yoff= self.statsdict['yoffset']
+        xoff = self.statsdict['xoffset']
+        yoff = self.statsdict['yoffset']
         self.image = self.images[foot][loc.direction]
         self.rect = self.image.get_rect()
         self.current_coord = loc
