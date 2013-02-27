3702cd2a97ef02a3870c05cce66664df02cc20fd
Kyle Rich <yutakafrog@gmail.com>
2013-02-27 10:49:53 -0700

    Make resting image have feet together

--- src/game.py
+++ src/game.py
@@ -184,12 +184,12 @@ class Game(object):
             statsdict['location'] = source
             statsdict['xoffset'] = int(completion * xdist)
             statsdict['yoffset'] = int(completion * ydist)
-            statsdict['foot'] = 0 if completion < 0.5 else 1
+            statsdict['foot'] = 1 if completion < 0.5 else 0
         else:
             statsdict['location'] = dest
             statsdict['xoffset'] = 0
             statsdict['yoffset'] = 0
-            statsdict['foot'] = 0
+            statsdict['foot'] = 1
             if self.pane.person[id].anim:
                 self.pane.person[id].anim.stop()
             self.pane.person[id].anim = None
