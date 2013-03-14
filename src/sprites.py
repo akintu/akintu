7ea26609700b6e00e03cd47f253132888f535ac9
Joshua Belcher <jabelch@gmail.com>
2013-03-13 23:45:36 -0600

    Added make transparent to Sprites

--- src/sprites.py
+++ src/sprites.py
@@ -91,6 +91,23 @@ class Sprites(object):
     @staticmethod
     def get_sheet(key):
         return Sprites.sheets[key]
+        
+    @staticmethod
+    def make_transparent(image, opacity=.5):
+    if opacity < 0:
+        opacity = 0
+    if opacity > 1:
+        opacity = 1
+        
+    if image.mode != 'RGBA':
+        image = image.convert('RGBA')
+    else:
+        image = image.copy()
+    alpha = image.split()[3]
+    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+    image.putalpha(alpha)
+    return image
+
 
 def crop_helper(const):
     return Crop(const[0], const[1], const[2], const[3], const[4])
