8c3addad1a778f7a7752a31d3cee183015f67abc
Colton Myers <colton.myers@gmail.com>
2013-03-15 15:23:51 -0600

    Add the ItemDialog class

--- src/screenutils.py
+++ src/screenutils.py
@@ -162,77 +162,252 @@ class TilingDialog(object):
             y += curfont.get_rect().height
         return surface
 
+
+class ItemDialog(object):
+    '''
+    Provides a two-paned, text-based dialog for use with inventory and shops
+    '''
+    def __init__(self,
+                 toptext,
+                 leftlist,
+                 rightlist,
+                 equipment=False,
+                 bgcolor='gray'):
+        '''
+        Initialize the class
+        '''
+        self.DEF_RECT = pygame.Rect(0, 0, 470, 30)
+        self.bgcolor = bgcolor
+        self.toptext = toptext
+        self.equipment = equipment
+        self.items = [leftlist, rightlist]
+        self.surface = pygame.Surface((1280, 640))
+        self.surface.fill(Color(self.bgcolor))
+        self.selection = (0, 0)
+        self.tops = [0, 0]
+
+        self.leftitems = self._generateitems(self.items[0])
+        self.rightitems = self._generateitems(self.items[1], self.equipment)
+
+        self._drawtoptext()
+        self._drawitems()
+        self._updateselection()
+        self._drawborders()
+
+        return self.selection
+
+    def set_items(self, leftlist, rightlist, toptext=None):
+        '''
+        Set the items lists (and optionally the top text), redraw the dialog
+        '''
+        self.items = [leftlist, rightlist]
+        self.leftitems = self._generateitems(self.items[0])
+        self.rightitems = self._generateitems(self.items[1], self.shop)
+        if len(self.leftitems) < 19:
+            self.tops[0] = 0
+        if len(self.rightitems) < 19:
+            self.tops[1] = 0
+        if toptext:
+            self.toptext = toptext
+            _drawtoptext()
+        self._drawitems()
+        # Recalculate the current position
+        self.move_selection(-1)
+        self._updateselection()
+
+        return self.selection
+
+    def move_selection(self, direction):
+        '''
+        Move the current selection (based on num pad directions, 2 4 6 8)
+        '''
+        selection = list(self.selection)
+        if direction == 2:
+            selection[1] += 1
+        elif direction == 4 or direction == 6:
+            selection[0] = (selection[0] + 1) % 2
+        elif direction == 8:
+            selection -= 1
+        elif direction == -1:
+            pass
+        else:
+            raise Exception('Invalid direction passed to '
+                            'TilingDialog.move_selection')
+        selection[1] = min(selection[1], len(self.items[selection[0]]) - 1)
+        selection[1] = max(selection[1], 0)
+        if self.selection != selection:
+            self.selection = tuple(selection)
+        self._updateselection()
+
+        return self.selection
+
+    def _updateselection():
+        '''
+        Draw a rectangle around current selection, removing from previous
+
+        Also error-checks the current selection (in case of out of bounds or
+        empty lists)
+        '''
+        if not self.selection:
+            return
+
+        # Error-checking
+        if len(items[self.selection[0]]) == 0:
+            self.selection = ((self.selection[0] + 1) % 2, 0)
+        if len(items[self.selection[0]]) == 0:
+            self.selection = None
+            return
+
+        # Calculate new tops
+        pane, item = self.selection
+        top = self.tops[pane]
+        if item < top:
+            top = item
+        elif item >= top + 19:
+            top = item - 18
+        self.tops[pane] = top
+
+        self._drawitems()
+
+        drawindex = item - top
+
+        x = 10 + pane * 830
+        y = 60 + 30 * drawindex
+        pygame.draw.rect(self.surface,
+                         Color('yellow'),
+                         self.DEF_RECT.move(x, y),
+                         3)
+        text = self.items[pane][item].details
+        self._drawsidetext(text)
+
+    def _drawitems(self):
+        '''
+        Draw the two lists
+        '''
+        y = 60
+        x = 10
+        for i in range(self.top[0],
+                       min(self.top[0] + 19, len(self.leftitems))):
+            self.surface.blit(self.leftitems[i], (x, y))
+            y += 30
+        y = 60
+        x = 840
+        for i in range(self.top[0],
+                       min(self.top[0] + 19, len(self.rightitems))):
+            self.surface.blit(self.rightitems[i], (x, y))
+            y += 30
+
+    def _drawtoptext(self):
+        '''
+        Draw the top text area
+        '''
+        textsurface = self._generatetext(self.toptext, height=30, widhth=1280)
+        self.surface.blit(textsurface, (20, 10))
+
+    def _drawsidetext(self, text):
+        '''
+        Draw the side text area with the given text
+        '''
+        textsurface = self._generatetext(text, )
+        self.surface.blit(textsurface, (490, 60))
+
+    def _drawborders(self):
+        '''
+        Draw the borders around the panes
+        '''
+        surface = self.surface
+        pygame.draw.line(surface, Color('black'), (0, 45), (1280, 45), 3)
+        pygame.draw.line(surface, Color('black'), (485, 45), (485, 640), 3)
+        pygame.draw.line(surface, Color('black'), (835, 45), (835, 640), 3)
+
+    def _generateitems(self, items, equipment=False):
+        surfaces = []
+
+        # Handle the equipment pane (if specified)
+        if equipment:
+            for item in items:
+                surfaces.append(self._generateitempane(item.name,
+                                                       item.type,
+                                                       item.weight,
+                                                       item.value))
+            for slot in ['Head', 'Legs', 'Feet', 'Chest', 'Hands', 'Neck']:
+                found = False
+                for item in items:
+                    if item.type == slot:
+                        found = True
+                        break
+                if found:
+                    continue
+                surfaces.append(self._generateemptyitempane(slot))
+            fingercount = 2
+            for item in items:
+                if item.type == 'Finger':
+                    fingercount -= 1
+            for i in range(0):
+                surfaces.append(self._generateemptyitempane('Finger'))
+            return surfaces
+
+        # Handle the normal pane
+        for item in items:
+            surfaces.append(self._generateitempane(item.name,
+                                                   item.type,
+                                                   item.weight,
+                                                   item.value))
+        if len(surfaces) == 0:
+            surfaces.append(self._generateemptyitempane())
+
+        return surfaces
+
+    def _generateitempane(self, name, itemtype, weight, value):
+        '''
+        Generate a given item pane
+        '''
+        surface = pygame.Surface((470, 30))
+        font = pygame.font.SysFont('Arial', 18)
+        bgcolor = Color(self.bgcolor)
+
+        weightstr = str(weight) + 'lbs'
+        valuestr = str(value) + 'g'
+
+        namefont = font.render(name, True, Color('black'), bgcolor)
+        typefont = font.render(itemtype, True, Color('black'), bgcolor)
+        weightfont = font.render(weightstr, True, Color('black'), bgcolor)
+        valuefont = font.render(valuestr, True, Color('black'), bgcolor)
+
+        surface.blit(namefont, (5, 4))
+        surface.blit(typefont, (295, 4))
+        surface.blit(weightfont, (385, 4))
+        surface.blit(valuefont, (425, 4))
+
+        return surface
+
+    def _generateemptyitempane(self, slot=''):
+        '''
+        Generate en empty item, including the slot text if provided
+        '''
+        surface = pygame.Surface((470, 30))
+        font = pygame.font.SysFont('Arial', 18)
+        curfont = font.render('Empty ' + slot, True, Color('black'))
+        surface.blit(curfont, (5, 4))
+        return surface
+
+    def _generatetext(self, text, height=590, width=360, color='black'):
+        '''
+        Generate a text surface of given height and width with the given text
+
+        Splits on newlines, does not wrap otherwise
+        '''
+        surface = pygame.Surface((width, height))
+        surface.fill(Color(self.bgcolor))
+        lines = text.split('\n')
+        y = 0
+        font = pygame.font.SysFont('Arial', 18)
+        for line in lines:
+            curfont = font.render(line, True, Color(color))
+            surface.blit(curfont, (0, y))
+            y += curfont.get_rect().height
+        return surface
+
+
 if __name__ == '__main__':
-    import time
-    pygame.init()
-
-    class TestItem(object):
-        def __init__(self, name, image, text):
-            self.name = name
-            self.image = image
-            self.text = text
-
-    screen = pygame.display.set_mode((1280, 640))
-    items = []
-    for i in range(44):
-        items.append(TestItem('Something of protection',
-                              'res/images/icons/cubeforce.png',
-                              'this is text\nplus some more text\n' +
-                              'seriously awesome text'))
-    dialog = TilingDialog('some text\nplus some more', items, 0, 'lightblue')
-    screen.blit(dialog.surface, (0, 0))
-    while True:
-        time.sleep(1)
-        dialog.move_selection(2)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(6)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(6)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(2)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(2)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(4)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(8)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(8)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
-        time.sleep(1)
-        dialog.move_selection(8)
-        print dialog.get_selection()
-        screen.blit(dialog.surface, (0, 0))
-        pygame.event.clear()
-        pygame.display.flip()
+    pass
