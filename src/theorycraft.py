cdd327cc0e5707eaf9cbb8d9e224f84fe2cd5c2f
BlackVegetable <devinj.ekins@gmail.com>
2013-03-26 20:42:34 -0600

    Fixed low lvl monsters joining

--- src/theorycraft.py
+++ src/theorycraft.py
@@ -303,9 +303,8 @@ class TheoryCraft(object):
                 selection = randint(0, len(subList) - 1)
             if subList[selection]['GP'] <= TheoryCraft.GROUP_GP - currentGP:
                 addingMonster = monster.Monster(subList[selection])
-                if initialMonster.level >= addingMonster.minLevel:
-                    addingMonster.setLevel(initialMonster.level)
-                listOfMonsters.append(monster.Monster(subList[selection]))
+                addingMonster.setLevel(initialMonster.level)
+                listOfMonsters.append(addingMonster)
                 currentGP += subList[selection]['GP']
                 numMonsters += 1
 
