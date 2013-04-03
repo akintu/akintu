f76b7fb62c256d990df5036ed97b0b7dcf0c64b1
Joshua Belcher <jabelch@gmail.com>
2013-04-02 18:15:27 -0600

    Buildings/Portal methods

--- src/portal.py
+++ src/portal.py
@@ -2,7 +2,8 @@
 from entity import*
 
 class Portal(Entity):
-    def __init__(self, new_location, location=None, team="Neutral", image=None, passable=True):
+
+    def __init__(self, new_location, location=None, type="Overworld", team="Neutral", image=None, passable=True):
         super(Portal, self).__init__(location=location, team=team, image=image, passable=passable)
         self.new_location = new_location
         
