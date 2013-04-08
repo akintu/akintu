e4d57762f3fe18dc22ecd5feb459e0e3eb0a693b
Joshua Belcher <jabelch@gmail.com>
2013-04-08 00:34:41 -0600

    Many changes to township and country

--- src/country.py
+++ src/country.py
@@ -1,5 +1,10 @@
 
+import pygame
+import random
+
 from const import*
+from region import*
+from township import*
 
 class Country(object):
     '''
@@ -8,6 +13,28 @@ class Country(object):
         COUNTRY_X, COUNTRY_Y defined in const.py
     '''
 
-    def __init__(self):
-        pass
-        
+    def __init__(self, seed, loc=(0,0)):
+        self.seed = seed
+        self.loc = loc
+
+        self.townships = dict()
+        for x in range(COUNTRY_X):
+            for y in range(COUNTRY_Y):
+                township = Township(self.seed, country_loc=loc, township_loc=(x,y))
+                self.townships[(x, y)] = township
+
+
+if __name__ == "__main__":
+    '''
+    Run some Tests
+    '''
+
+    for x in range(2):
+        for y in range(1):
+            c = Country("SOME NEW SEED", (x,y))
+            print "_"*40
+            print "COUNTRY: " + str((x, y))
+            for loc, township in c.townships.iteritems():
+                print "   Township: " + str(township.loc)
+                print "\tTopLeft Pane: " + str(township.topLeft)
+                print "\tCenter Pane: " + str(township.center)
