b8c31357568e1974d22bb1faea23c32c2c851794
Joshua Belcher <jabelch@gmail.com>
2013-04-18 21:12:40 -0600

    Monster level capped at 9

--- src/pane.py
+++ src/pane.py
@@ -126,8 +126,7 @@ class Pane(object):
             location = Location(self.location, (random.randrange(PANE_X), random.randrange(PANE_Y)))
 
         ignore_max = True if level > 9 else False
-        
-        level = 15
+
         level = min(level, 9) if name != "Devil" else level
         # print ignore_max
         person = TheoryCraft.getMonster(level=level, name=name, tolerance=tolerance, ignoreMaxLevel=ignore_max)
